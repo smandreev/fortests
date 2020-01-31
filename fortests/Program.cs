9450c583eff662928be1ce78d80d5fe1d1b4d215
Sergey.Andreev <sergey.andreev@jetbrains.com>
2020-01-31 17:37:59 +0300

    Initial solution

---
+++ fortests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fortests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
